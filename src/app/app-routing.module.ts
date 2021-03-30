7a9336e7d5cfd79e5b0995968f228bd3d9665218
Jeshwanth <jeshwanth.p@cloudcollab.co>
2021-03-30 17:22:12 +0530

    login and register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginpageComponent } from './apps/loginpage/loginpage.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ForgetpasswordComponent } from './forgetpassword/forgetpassword.component';
+import { RegisterpageComponent } from './registerpage/registerpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'/login',
+    pathMatch:'full'
+  },
+  {
+    path: 'login',
+    component: LoginpageComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'registration',
+    component: RegisterpageComponent
+  },
+  {
+    path: 'change-password',
+    component: ForgetpasswordComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LoginpageComponent } from './apps/loginpage/loginpage.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RegisterpageComponent } from './registerpage/registerpage.component';
+import { ForgetpasswordComponent } from './forgetpassword/forgetpassword.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginpageComponent,
+    DashboardComponent,
+    RegisterpageComponent,
+    ForgetpasswordComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
